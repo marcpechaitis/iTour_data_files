5d06b2e34f5bc9046d2c5912c67180451a206888
Marc Pechaitis <marcpechaitis@gmail.com>
2017-01-18 12:43:43 -0700

    chore(): add params files for backups

---
+++ params/params SCI.js
@@ -0,0 +1,22 @@
+const params = {
+	APP_NAME: 'iTour.phish FREE',
+	BAND: 'PHISH',
+	BAND_NAME: 'Phish',
+	BAND_URL_DEFAULT: 'http://phish.com/from-the-road',
+	MUSIC_TITLE: 'JamBase Local Music',
+	MUSIC_URL_NON_USA: 'http://www.jambase.com',
+	MUSIC_URL1: 'http://www.jambase.com/shows?mode=all&start=',
+	MUSIC_URL2: '&address=',
+	MUSIC_URL3: '&region=&country=&radius=60',
+	SEATING_TITLE: 'Seating Chart',
+	SETLIST_TITLE: 'Phish.net Setlist',
+	TICKETS_TITLE: 'CashorTrade Tickets',
+	TICKETS_URL: 'http://m.cashortrade.org/phish-tickets',
+	UPGRADE_URL: 'itms://itunes.apple.com/us/app/apple-store/id391888525',
+	VIDEO_TITLE: 'YouTube Videos',
+	VIDEO_URL: 'https://m.youtube.com/#/results?q=phish%20',
+	WEATHER_TITLE: 'Dark Sky Weather',
+	WEATHER_URL: 'https://darksky.net/'
+}
+
+module.exports = params;
